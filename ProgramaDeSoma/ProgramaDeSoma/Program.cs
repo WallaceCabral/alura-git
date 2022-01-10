8a4da1e93f8961f472226a85f11ec18bca727036
Wallace Cabral <wallacecabral.carlos@gmail.com>
2022-01-10 10:14:22 -0300

    Fibonacci-teste

---
+++ ProgramaDeSoma/ProgramaDeSoma/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+int n1, n2, resultado;
+
+Console.WriteLine("Digite dois valores inteiros para soma");
+n1 = int.Parse(Console.ReadLine());
+n2 = int.Parse(Console.ReadLine());
+
+resultado = n1 + n2;
+
+    Console.WriteLine($"A soma dos valores é {resultado}");
